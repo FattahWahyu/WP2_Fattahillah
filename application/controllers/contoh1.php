58c5085719097eed720193821c35c44612df27ba
FattahWahyu <fattahillahwahyu14@gamil.com>
2022-09-22 14:56:56 +0700

    CI pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Fattahillah Wahyu Saya tinggal di daerah Klaten olah raga yang saya sukai adalah Sepakbola";
+    }
+}
