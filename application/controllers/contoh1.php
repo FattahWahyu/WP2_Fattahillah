87c2ffe75ba09aca4793f88285f58c08aafe2343
FattahWahyu <fattahillahwahyu14@gamil.com>
2022-09-23 15:26:01 +0700

    cek

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,6 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Fattahillah Wahyu Saya tinggal di daerah Klaten olah raga yang saya sukai adalah Sepakbola";
+        echo"Nama saya Fattahillah Wahyu
+            Saya tinggal di daerah Klaten
+            saya suka bermain game mobile";
     }
 }
