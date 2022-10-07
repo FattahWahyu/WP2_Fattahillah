314b0ce3c818307d977f27300aa5d7b9ccd26116
FattahWahyu <fattahillahwahyu14@gamil.com>
2022-10-07 15:47:33 +0700

    Menambah about

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fattahillah</li>
+        <li>Nama Belakang : Wahyu</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Taji, Juwiring, Klaten</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Klaten</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Sepak Bola</li>
+        <li>Voli</li>
+    </ul>
+</section>
